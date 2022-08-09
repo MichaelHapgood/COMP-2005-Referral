ecbc1a09087247b09e674266fe075bfe59221627
Mikron93 <mhapgood93@hotmail.co.uk>
2022-08-09 11:13:35 +0100

    Added variables

--- SoftwareTesting/COMP2005.java
+++ SoftwareTesting/COMP2005.java
@@ -1,15 +1,15 @@
 // Java program to read JSON from a file
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Scanner;
-
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.LinkedHashMap;
+import java.util.Map;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
-import org.json.simple.parser.*;
+import org.json.simple.parser.JSONParser;
 
 public class COMP2005 {
     private static String inline;
@@ -20,22 +20,31 @@ public class COMP2005 {
      */
     public static void main(String[] args) throws Exception {
 
-        URL url = new URL("http://intelligent-social-robots-ws.com/senators.json");
-        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-        conn.setRequestMethod("GET");
-        conn.connect();
-        int responsecode = conn.getResponseCode();
-        if (responsecode != 200)
-            throw new RuntimeException("HttpResponseCode: " + responsecode);
-        else {
-            Scanner sc = new Scanner(url.openStream());
-            while (sc.hasNext()) {
-                inline += sc.nextLine();
-            }
-            System.out.println("\nJSON data in string format");
-            System.out.println(inline);
-            sc.close();
-        }
+        try {
+            URL url = new URL("http://intelligent-social-robots-ws.com/senators.json");
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.connect();
+            if (conn.getResponseCode() == 200) {
+                Scanner scan = new Scanner(url.openStream());
+                while (scan.hasNext()) {
+                    String temp = scan.nextLine();
+                    Object obj = new JSONParser().parse((temp));
 
+                    // typecasting obj to JSONObject
+                    JSONObject jo = (JSONObject) obj;
+
+                    // getting firstName and lastName
+                    String firstName = (String) jo.get("firstName");
+                    String lastName = (String) jo.get("lastName");
+
+                    System.out.println(firstName);
+                    System.out.println(lastName);
+                }
+            }
+
+        } catch (FileNotFoundException e) {
+            throw new FileNotFoundException("Error.");
+        }
     }
 }
